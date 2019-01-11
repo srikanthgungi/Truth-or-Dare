65e152a4e5c9fc9cee56ec9e60cc303ca282fa4e
srikanthgungi <srikanth.gsk@outlook.com>
2019-01-11 08:02:06 -0600

    Add files via upload

---
+++ familyGame.py
@@ -0,0 +1,21 @@
+import random
+def truthOrdareGame(number):
+    if number==0:
+        return "its dad"
+    elif number==1:
+        return "its mom"
+    elif number==2:
+        return "its anna"
+    elif number==3:
+        return "its srikanth"
+    elif number==4:
+        return "its Harshika"
+    elif number==5:
+        return "its vadhina"
+    elif number==6:
+        return "its Harshika"
+    elif number==7:
+        return "its Midhun"
+r=random.randint(0,7)
+turn=truthOrdareGame(r)
+print(turn)
